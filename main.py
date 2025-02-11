f698e9ff26def3a34d1f80373bc83ce0366eb651
tokizuoh <tokizuoh@gmail.com>
2025-02-11 15:34:21 +0900

    Use repository

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import openai
 from dotenv import load_dotenv
+from repository import Repository
 
 if __name__ == "__main__":
     load_dotenv()
+    
+    repo = Repository()
+    proposals = repo.get_recently_changed_proposals()
+    for title, content in proposals.items():
+        print(f"{title}\n")
+        print(content[:1000])
+        print("\n" + "=" * 50 + "\n")
+        
+    exit()
+    # WIP
 
     client = openai.OpenAI()
     response = client.chat.completions.create(
